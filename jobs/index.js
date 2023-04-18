5b7df479064b65f7b44957c4618222893a95bdaf
KMMRCap <mortezarezainia@gmail.com>
2023-04-18 14:55:27 +0330

    nodes routes deleted - cors whitelist deleted - custom 404 and 500 added (commented) - port changed to 8004 - logger added with morgan

--- jobs/index.js
+++ jobs/index.js
@@ -2,7 +2,6 @@ import Bree from 'bree'
 
 export const initBreeInstance = () => {
     const bree = new Bree({
-        // logger: false,
         jobs: [
             {
                 name: 'updateRequestsHistory',
