a8d4234f57412708baf80c591d79cc51637d6ac0
KMMRCap <mortezarezainia@gmail.com>
2023-06-21 16:18:29 +0330

    bug fix : error handlers commented - requests history bug fixed

--- server.js
+++ server.js
@@ -44,20 +44,20 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 8004
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port: ", PORT)
 })
 
-process.on('uncaughtException', err => {
-    console.log(`Error: ${err.stack}`);
-    console.log('Shutting down due to uncaught exception');
-    process.exit(1)
-})
+// process.on('uncaughtException', err => {
+//     console.log(`Error: ${err.stack}`);
+//     console.log('Shutting down due to uncaught exception');
+//     process.exit(1)
+// })
 
-process.on('unhandledRejection', err => {
-    console.log(`Error: ${err.stack}`)
-    console.log(`Shutting down the server due to unhandled Promise rejection`)
-    server.close(() => {
-        process.exit(1)
-    })
-})
+// process.on('unhandledRejection', err => {
+//     console.log(`Error: ${err.stack}`)
+//     console.log(`Shutting down the server due to unhandled Promise rejection`)
+//     server.close(() => {
+//         process.exit(1)
+//     })
+// })
