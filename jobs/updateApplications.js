9ed9bd2ea57f26d09539cc8cf6b4a38b38610267
KMMRCap <mortezarezainia@gmail.com>
2023-08-01 12:17:02 +0330

    update : app cron job changed to also update apps data from json file

--- jobs/updateApplications.js
+++ jobs/updateApplications.js
@@ -19,15 +19,40 @@ if (parentPort) {
 
     const staticApps = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/apps.json')))
 
-    let apps = await db.collection('applications').find().toArray()
+    const appsToAdd = staticApps.map(i => ({ ...i, data: {} }))
+    const createPromises = appsToAdd.map(async app => {
+        const promise = new Promise(async resolve => {
+            await db.collection('applications').findOneAndReplace({ id: app.id }, app, { upsert: true })
+            resolve()
+        })
+        return promise
+    })
 
-    if (staticApps.length !== apps.length) {
-        const appsToAdd = staticApps.map(i => ({ ...i, data: {} }))
-        await db.collection('applications').insertMany(appsToAdd)
-        apps = await db.collection('applications').find().toArray()
+    await Promise.all(createPromises)
+
+    const apps = await db.collection('applications').find().toArray()
+
+    if (apps.length !== appsToAdd.length) {
+        let ids = []
+        apps.forEach(item => {
+            const found = appsToAdd.find(i => i.id === item.id)
+            if (!found) {
+                ids.push(item.id)
+            }
+        })
+        const deletePromises = ids.map(async id => {
+            const promise = new Promise(async resolve => {
+                await db.collection('applications').deleteOne({ id })
+                resolve()
+            })
+            return promise
+        })
+        await Promise.all(deletePromises)
     }
 
-    const promises = apps.map(async app => {
+    const correctApps = await db.collection('applications').find().toArray()
+
+    const updatePromises = correctApps.map(async app => {
         const promise = new Promise(async resolve => {
             try {
                 const { data } = await axios.get(`https://alice.muon.net/v1/?app=explorer&method=app&params[appName]=${app.id}`)
@@ -47,7 +72,7 @@ if (parentPort) {
         return promise
     })
 
-    await Promise.all(promises)
+    await Promise.all(updatePromises)
 
     if (parentPort) parentPort.postMessage('done');
     else process.exit(0);
